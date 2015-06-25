73c3e561c275c7045c53e5c34606e2de5858fe6f
Mickael <mrabouille@users.noreply.github.com>
2015-06-25 10:29:37 -0300

    Add reference: Elmar Plischke (2010)

--- rbdfast.m
+++ rbdfast.m
@@ -26,18 +26,22 @@ function [SIc,SI] = rbdfast(X,Y,Index,M)
 %Joint Research Centre All rights Reserved
 %
 %Update: M. Rabouille
-%Add: Reordering Y according a random design X.
-%Add: Unbiased estimator from Tissot & Prieur.
+%Add: Reordering Y according a random design X (EASI algorithm) from E Plischke.
+%Add: Unbiased estimator from J-Y Tissot & C Prieur.
 %Note: The estimate is less dependant on the M value which can be raised up to 10.
 %
 %References: 
-%Tarantola S., Gatelli, D. and T. Mara (2006) 
+%S. Tarantola, D. Gatelli and T. Mara (2006)
 %Random Balance Designs for the Estimation of First Order 
 %Global Sensitivity Indices, Reliability Engineering and System Safety, 91:6, 717-727
 %
+%Elmar Plischke (2010)
+%An effective algorithm for computing global sensitivity indices (EASI)
+%Reliability Engineering & System Safety, 95:4, 354-360. <10.1016/j.ress.2009.11.005>
+%
 %Jean-Yves Tissot, Clémentine Prieur (2012)
 %Bias correction for the estimation of sensitivity indices based on random balance designs.
-%Reliability Engineering and System Safety, Elsevier,  107, 205-213. <10.1016/j.ress.2012.06.010>. <hal-00507526v2>
+%Reliability Engineering and System Safety, Elsevier,  107, 205-213. <10.1016/j.ress.2012.06.010> <hal-00507526v2>
 
 
 % Test function
